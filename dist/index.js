2e7ed33afff9cf929448c2b11d5884a52bcc9cf2
John Tseng <zengsy@hotmail.com>
2017-12-08 16:04:50 +0800

    include dist folder for npm publish

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _store = require('./store');Object.defineProperty(exports, 'withStore', { enumerable: true, get: function get() {return _store.
+        withStore;} });Object.defineProperty(exports, 'withActions', { enumerable: true, get: function get() {return _store.
+        withActions;} });Object.defineProperty(exports, 'registryService', { enumerable: true, get: function get() {return _store.
+        registryService;} });Object.defineProperty(exports, 'pushChange', { enumerable: true, get: function get() {return _store.
+        pushChange;} });Object.defineProperty(exports, 'callAction', { enumerable: true, get: function get() {return _store.
+        callAction;} });
